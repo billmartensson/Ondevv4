15a9f87a40c50f1a2b6df789526d334efa63224d
Bill Mårtensson <bill@magictechnology.se>
2024-11-06 09:52:51 +0100

    Initial Commit

---
+++ Ondevv4/Ondevv4App.swift
@@ -0,0 +1,17 @@
+//
+//  Ondevv4App.swift
+//  Ondevv4
+//
+//  Created by BillU on 2024-11-06.
+//
+
+import SwiftUI
+
+@main
+struct Ondevv4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
